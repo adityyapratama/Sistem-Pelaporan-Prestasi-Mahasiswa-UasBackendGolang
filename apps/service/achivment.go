4b1aad71812ac1a8ba9dcab347a90ab0a9ee956b
Aditya Pratama <adityavalo333@gmail.com>
2025-12-16 09:33:36 +0700

    add generate token

--- apps/service/achivment.go
+++ apps/service/achivment.go
@@ -1,29 +1,32 @@
 package service
 
-import "C"
-import (
-	"net/http"
-	"uas-pelaporan-prestasi-mahasiswa/apps/repository"
-
-	"github.com/gofiber/fiber/v2"
-)
-
-type AchievementService struct {
-	achievementRepo repository.AchievementRepository
-	studentRepo     repository.StudentsRepository
-}
-
-func NewAchivmentService(achievementRepo repository.AchievementRepository, studentRepo repository.StudentsRepository) *AchievementService {
-	return &AchievementService{
-		achievementRepo: achievementRepo,
-		studentRepo:     studentRepo,
-	}
-}
-
-func (s *AchievementService) Create(fiber fiber.Ctx) {
-	var req models.Achievement
-	if err := c.BodyParser(fiber, &req); err != nil {
-		fiber.Status(http.StatusBadRequest)
-
-	}
-}
+//
+//import "C"
+//import (
+//	"net/http"
+//	"uas-pelaporan-prestasi-mahasiswa/apps/repository"
+//
+//	"github.com/gofiber/fiber/v2"
+//)
+//
+//type AchievementService struct {
+//	achievementRepo repository.AchievementRepository
+//	studentRepo     repository.StudentsRepository
+//}
+//
+//func NewAchivmentService(achievementRepo repository.AchievementRepository, studentRepo repository.StudentsRepository) *AchievementService {
+//	return &AchievementService{
+//		achievementRepo: achievementRepo,
+//		studentRepo:     studentRepo,
+//	}
+//}
+//
+//func (s *AchievementService) Create(fiber fiber.Ctx) {
+//	var req models.Achievement
+//	if err := c.BodyParser(fiber, &req); err != nil {
+//		fiber.Status(http.StatusBadRequest)
+//
+//	}
+//
+//
+//}
